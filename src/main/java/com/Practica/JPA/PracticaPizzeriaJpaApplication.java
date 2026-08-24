14bf4655b8859d6789b1416e9193e2ea310e135e
Jampi0327 <pioncegredy@outlook.com>
2026-08-23 21:56:32 -0500

    feat: configuracion inicial spring boot con gradle

---
+++ src/main/java/com/Practica/JPA/PracticaPizzeriaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.Practica.JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaPizzeriaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaPizzeriaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practica/JPA/PracticaPizzeriaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaPizzeriaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
